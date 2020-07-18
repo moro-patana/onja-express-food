35bd8bb92c8355b393766fc3f73e6dfd36ef3937
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-07-18 11:28:42 +0300

    added event listener to the button

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('good luck!');
+const container = document.querySelector(`.container`);
+const btn = document.querySelector(`.add-order`);
+const outerModal = document.querySelector(`.outer-modal`);
+const innerModal = document.querySelector(`.inner-modal`);
+
+const form = document.getElementsByTagName(`form`)[0];
+
+
+
+btn.onclick = (e) => {
+    outerModal.style.display = `block`;
+    container.style.opacity = `0.5`;
+};
+form.onclick = function() {
+    outerModal.style.display = `none`;
+}
+window.onclick = function(event) {
+    if (event.target === outerModal) {
+        outerModal.style.display = `none`;
+    }
+}
